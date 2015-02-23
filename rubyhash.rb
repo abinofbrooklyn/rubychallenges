b6860ff8d80e22912cb8465869905faf4675528d
Abin <abin.abraham4@gmail.com>
2015-02-23 17:13:34 -0500

    ruby hash

--- rubyhash.rb
+++ rubyhash.rb
@@ -1,3 +1,40 @@
 class RubyHash
   attr_accessor :instruments
+
+  def initialize(instruments)
+    @instruments = instruments
+  end
+
+  def switch_key_value
+    @instruments.invert
+  end
+
+  def return_values
+    @instruments.values
+  end
+
+  def return_keys
+    @instrument.keys
+  end
+
+  def replace_value(key, value)
+    @instruments[key] = value
+  end
+
+  def add_to_hash(key, value)
+    @instruments.merge!(key => value)
+  end
+
+  def length_of_hash
+    @instruments.length
+  end
+
+  def find_key_with_same_values(value)
+    @instruments.select {|k, v| v == value }.keys
+  end
+
+  def key_value_to_sentence(key)
+    value = @instruments.select {|k, v| k == key }.values
+    "#{key.capitalize}'s value is #{value.join(" ")}"
+  end
 end
--- spec/rubyhash_spec.rb
+++ spec/rubyhash_spec.rb
@@ -3,11 +3,37 @@ require_relative '../rubyhash'
 
 describe RubyHash do
   before do
-    @my_ruby = RubyHash.new( :cello     => 'string',
-                             :clarinet  => 'woodwind',
-                             :drum      => 'percussion',
-                             :oboe      => 'woodwind',
-                             :trumpet   => 'brass',
-                             :violin    => 'string' )
+    @my_ruby = RubyHash.new(cello: 'string', clarinet: 'woodwind', drum: 'percussion', oboe: 'woodwind', trumpet: 'brass', violin: 'string')
+  end
+
+  it "switches the keys and values of the hash" do
+    switch_hash = @my_ruby.switch_key_value
+    expect(@my_ruby.switch_key_value).to eq(switch_hash)
+  end
+
+  it "replaces the value of a specfic key" do
+    expect(@my_ruby.replace_value(:cello, 'bass')).to eq('bass')
+  end
+
+  it "returns all the values of the hash" do
+    values = @my_ruby.return_values
+    expect(@my_ruby.return_values).to eq(values)
+  end
+
+  it "finds all the keys with the same values" do
+    expect(@my_ruby.find_key_with_same_values("string")).to include(:cello, :violin)
+  end
+
+  it "adds a key and value to the hash" do
+    expect(@my_ruby.add_to_hash(:flute, "woodwind")).to include(:flute => 'woodwind')
+  end
+
+  it "counts key value pair of hash" do
+    @my_ruby.add_to_hash(:flute, "woodwind")
+    expect(@my_ruby.length_of_hash).to eq 7
+  end
+
+  it "prints a sentence with key and value" do
+    expect(@my_ruby.key_value_to_sentence(:cello)).to eq("Cello's value is string")
   end
 end
